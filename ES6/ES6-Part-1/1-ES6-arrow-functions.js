32f601810f2132a0def9afbe616544f3b5137d1e
Konstantinos Stamatopoulos <v-kostam@microsoft.com>
2017-11-03 22:11:04 +0200

    add content for hoisting variables fix index html to reference the renamed files

--- ES6/ES6-Part-1/1-ES6-arrow-functions.js
+++ ES6/ES6-Part-1/1-ES6-arrow-functions.js
@@ -1,9 +1,35 @@
+function hoist() {
+    a = 20; //because we don't use var the a variable is assigned to global scope
+    var b = 100;
+}
+
+hoist();
+
+console.log("hoist function variable a", a);
+
+//Accessible as a global variable outside hoist() function
+//Output: 20
+
+//console.log("hoist function variable b", b);
+
+//Since it was declared, it is confined to the hoist() function scope.
+//We can't print it out outside the confines of the hoist() function.
+//Output: ReferenceError: b is not defined
+
+
+//----------------------------------//
+
+
 let profileId = 12;
 {
     let profileId = 152;
 }
 //console log is 12 because of the block scope of let and const keyword
 console.log("ProfileId:", profileId);
+
+
+//----------------------------------//
+
 
 document.addEventListener('click', function () {
     console.log("EventListener with classic function", this); //it will show #document, the item the event is attached.
@@ -12,6 +38,8 @@ document.addEventListener('click', function () {
 document.addEventListener('click', () => {
     console.log("EventListener with arrow function", this); //it will show Window, arrow functions, inherit the context in which they are running.
 })
+
+//----------------------------------//
 
 let payment =
     {
@@ -33,6 +61,8 @@ let payment2 =
 
 payment2.execute();
 
+//----------------------------------//
+
 //ATTENTION
 //You will not be able to change the context of "this" using bind or call with arrow functions.
 //You won't receive any error it just won't work :(
@@ -41,9 +71,16 @@ payment2.execute();
 let order = {
     id: 5,
     process: function () {
-        return () =>  { console.log("Process the order with Id:", this.id)};
+        return () => { console.log("Process the order with Id:", this.id) };
     }
 };
+
+// let order = {
+//     id: 5,
+//     process: function () {
+//         return function() { console.log("Process the order with Id:", this.id) };
+//     }
+// };
 
 let order2 = {
     id: 22,
