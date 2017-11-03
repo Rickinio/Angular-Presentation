f60a94434b5937124718e65238ee9785a304333c
Konstantinos Stamatopoulos <v-kostam@microsoft.com>
2017-11-03 22:34:16 +0200

    add examples of const

--- ES6/ES6-Part-1/1-ES6-arrow-functions.js
+++ ES6/ES6-Part-1/1-ES6-arrow-functions.js
@@ -1,21 +1,75 @@
+/*
+Functions and Variable hoisting:
+If a variable or a function is declared then javascript internaly moves them on top of the document
+and it appears that you can use them before the are declared.
+Be carefull if you initialize the variable or assign the function to a variable then,
+they are not moved at the top at runtime and you'll get an exception
+*/
+
+//In the below example hoist function is actually moved on top when javascript parse the file
+//and so i can hoist() before declaring it.
+//in case i use var hoist = function() then it won't work
+
+hoist();
+
+//Accessible as a global variable outside hoist() function
+//Output: 20
+console.log("hoist function variable a", a);
+
 function hoist() {
     a = 20; //because we don't use var the a variable is assigned to global scope
     var b = 100;
 }
 
-hoist();
-
-console.log("hoist function variable a", a);
-
-//Accessible as a global variable outside hoist() function
-//Output: 20
-
+//The below will throw an exception ReferenceError: b is not defined 
 //console.log("hoist function variable b", b);
 
 //Since it was declared, it is confined to the hoist() function scope.
 //We can't print it out outside the confines of the hoist() function.
 //Output: ReferenceError: b is not defined
 
+
+//----------------------------------//
+
+/*
+The difference of var and let keyword is scoping.
+var is scoped to the nearest function block and let is scoped to the nearest enclosing block,
+which can be smaller than a function block. Both are global if outside any block.
+*/
+
+let me = 'go';  // globally scoped
+var i = 'able'; // globally scoped
+
+console.log(window.me); // undefined cause variables declared with let in global scope, the don't get added in window object
+console.log(window.i); // 'able'
+
+function iterateWithLet() {
+    //t is *not* visible out here
+    console.log('t value before loop:', t);
+
+    for (let t = 0; t < 5; t++) {
+        //t is only visible in here (and in the for() parentheses)
+        //and there is a separate t variable for each iteration of the loop
+    }
+
+    //t is *not* visible out here
+    console.log('t value after loop:', t);
+}
+
+function iterateWithVar() {
+    //z *is* visible out here
+    console.log('z value before loop:', z);
+
+    for (var z = 0; z < 5; z++) {
+        //z is visible to the whole function
+    }
+
+    //z *is* visible out here
+    console.log('z value after loop:', z);
+}
+
+//iterateWithLet(); //this will throw ReferenceError: t is not defined
+iterateWithVar();
 
 //----------------------------------//
 
@@ -26,6 +80,13 @@ let profileId = 12;
 }
 //console log is 12 because of the block scope of let and const keyword
 console.log("ProfileId:", profileId);
+
+const userId = 11;
+//userId = 12; //will throw and error
+
+const user = {name:'John',age:23};
+user.name = 'Kostas'; //this will work, i can change the properies of a const object but i can't assign it to another object
+//user = {name:'Alice',age:30}; //will throw exception
 
 
 //----------------------------------//
