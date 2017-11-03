a263091e5fac202eb57b00a8231b8eb52366a522
Konstantinos Stamatopoulos <v-kostam@microsoft.com>
2017-11-03 22:55:15 +0200

    Add more comments in rest and spread operators

--- ES6/ES6-Part-1/3-ES6-rest-spread.js
+++ ES6/ES6-Part-1/3-ES6-rest-spread.js
@@ -1,13 +1,21 @@
+/*
+The rest parameter syntax allows us to represent an indefinite number of arguments as an array.
+ATTENTION:Rest parameter must be last formal parameter of the function
+*/
+
 let fundingFn = function (startup, ...funding) {
+    console.log("startup:", startup);
     console.log("Funding from (array):", funding);
     console.log("Funding from (spread):", ...funding);
 }
 
-fundingFn("awesomeStartup", "Angel Investors", "Espa");
-
-//The rest parameter syntax allows us to represent an indefinite number of arguments as an array.
+fundingFn("awesomeStartup", "Entrepreneur", "Espa");
 
 //----------------------------------//
+
+/*
+You can use spread operator on an array to easily copy it to a new one
+*/
 
 let heroes = ["Batman", "Superman", "Catwoman"];
 let newHeroes = [...heroes];
@@ -17,14 +25,20 @@ console.log("Heroes:", ...heroes);
 console.log("New Heroes:", newHeroes);
 console.log("New Heroes plus Groot:", newHeroesPlusGroot);
 
-//We can use the spread syntax to copy an array to a new one
-
 //----------------------------------//
 
+/*
+You can use rest operator to pass the values of an array as arguments to a function
+*/
 var prices = [12, 20, 1, 100];
+console.log("Max price:", Math.max(1, 2, 3, 4));
 console.log("Max price:", Math.max(...prices));
 
+
 //----------------------------------//
 
+/*
+You can use spread operator break string into chars, no need to use split('') PROFIT :P
+*/
 var alphabet = ["A", ..."BCD", "E"];
 console.log(alphabet);
