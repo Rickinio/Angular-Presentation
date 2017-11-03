0348a8d77d942f43bdc3a58f06d1ee2b03cb3604
Konstantinos Stamatopoulos <v-kostam@microsoft.com>
2017-11-03 23:18:55 +0200

    Add examples of Map and Set

--- ES6/ES6-Part-1/4-ES6-object-literals.js
+++ ES6/ES6-Part-1/4-ES6-object-literals.js
@@ -1,27 +1,36 @@
+/*
+There is a new shorthand to declair objects,
+when the value variable of an object has the same name as the key it can be ommited.
+*/
+
 let engine = "turbo";
 let gear = "manual";
+
+let oldSyntaxCar = {
+    engine: engine,
+    gear: gear
+};
 
 let car = {
     engine,
     gear
 };
 
-//there is a new shorthand to declair objects
-//when the value variable of an object has the same name as the key it can be ommited.
-
 console.log("Car:", car);
 
 //----------------------------------//
 
-//Use variable value as key of an object
+/*
+You can now easily use a **computed property** as key of an object
+*/
 
 let engineKey = "engine";
 let gearKey = "gear";
 
 let car2 = {
-    [engineKey] : "simple",
-    [gearKey + "002"]:"automatic",
-    printDescription(){
+    [engineKey]: "simple",
+    [gearKey + "002"]: "automatic",
+    printDescription() {
         return "Car description: " + this.engine + ' ' + this.gear;
     }
 };
@@ -30,15 +39,19 @@ console.log(car2);
 
 //----------------------------------//
 
+/*
+A property of an object can also refer to a function or a getter or setter method.
+*/
+
 let carType = "isCabrio";
 
 let car3 = {
-    get [carType] () {
+    get [carType]() {
         return true;
     },
-    set [carType] (value){
-
-    }
+    set [carType](value) {
+       
+    },    
 }
 
-console.log("Is cabrio? ",car3.isCabrio);
+console.log("Is cabrio? ", car3.isCabrio);
--- ES6/ES6-Part-1/5-ES6-for-of-loops.js
+++ ES6/ES6-Part-1/5-ES6-for-of-loops.js
@@ -15,3 +15,41 @@ let letters = "ABCD";
 for (let l of letters) {
     console.log(l);
 }
+
+
+//----------------------------------//
+
+/*
+The Map object holds key-value pairs. Any value (both objects and primitive values) may be used as either a key or a value.
+*/
+
+let map = new Map([['a', 1], ['b', 2], ['c', 3]]);
+
+for (let entry of map) {
+  console.log('Map key value pair:',entry);
+}
+// ['a', 1]
+// ['b', 2]
+// ['c', 3]
+
+for (let [key, value] of map) {
+  console.log('Map value',value);
+}
+// 1
+// 2
+// 3
+
+//----------------------------------//
+
+/*
+The Set object lets you store unique values of any type, whether primitive values or object references.
+*/
+
+let set = new Set([1, 1, 2, 2, 3, 3]);
+
+for (let value of set) {
+  console.log('Set value:',value);
+}
+// 1
+// 2
+// 3
